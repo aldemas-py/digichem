11be3507d67272feb7ac5241d2ec80915dccd004
aldemas-py <njengasam2406@gmail.com>
2022-04-24 08:53:35 +0300

    xhtml page created

--- js/ext.js
+++ js/ext.js
@@ -1,3 +1,20 @@
+// footer
+function footerAlign() {
+    var footerHeight = $(".footer").outerHeight();
+    $("body").css("padding-bottom", footerHeight);
+}
+$(document).ready(function () {
+    // body
+    obj = $("#homeImage");
+    pgheight = $(window).height();
+    obj.height = obj.css("height", pgheight * 0.7);
+    // footer
+    footerAlign();
+    // $(".footer").html(htmlString);
+});
+$(window).resize(function () {
+    footerAlign();
+});
 // Navbar
 const nav = document.querySelector(".navbar");
 fetch("header/navbar.html")
@@ -6,25 +23,18 @@ fetch("header/navbar.html")
         nav.innerHTML = data;
     });
 
+function myFunction() {
+    var x = document.getElementById("myNavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
 // footer
-
 const foot = document.querySelector(".footer");
 fetch("header/footer.html")
     .then((res) => res.text())
     .then((data) => {
         foot.innerHTML = data;
     });
-
-function footerAlign() {
-    var footerHeight = $(".footer").outerHeight();
-    $("body").css("padding-bottom", footerHeight);
-}
-
-$(document).ready(function () {
-    footerAlign();
-    $(".footer").html(htmlString);
-});
-
-$(window).resize(function () {
-    footerAlign();
-});
